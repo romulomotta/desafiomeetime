d59989cba1254d918991448e84f5698d54d5b8f0
romulomotta <romulomotta@outlook.com>
2025-04-30 22:57:58 -0300

    Cria log para webhook de Contact

--- src/main/java/com/lobato/desafiomeetime/application/service/ContactService.java
+++ src/main/java/com/lobato/desafiomeetime/application/service/ContactService.java
@@ -2,12 +2,19 @@ package com.lobato.desafiomeetime.application.service;
 
 import com.lobato.desafiomeetime.application.ContactMapper;
 import com.lobato.desafiomeetime.application.domain.ContactRequestDomain;
+import com.lobato.desafiomeetime.entrypoint.dto.ContactNotificationRequestDto;
 import com.lobato.desafiomeetime.entrypoint.dto.ContactResponseDto;
 import com.lobato.desafiomeetime.repository.Integration.ContactIntegration;
+import com.lobato.desafiomeetime.repository.entity.ContactResponseEntity;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ContactService {
+    private static final Logger LOGGER = LoggerFactory.getLogger(ContactService.class);
     private final ContactIntegration integration;
     private final AuthService authService;
     private final ContactMapper mapper;
@@ -20,10 +27,14 @@ public class ContactService {
 
     public ContactResponseDto createContact(ContactRequestDomain request) {
 
-        return mapper.toDto(integration.createContact(request));
+        ContactResponseEntity response = integration.createContact(request);
+        LOGGER.info("Contato [%s] criado com sucesso.".formatted(response.properties().firstname()));
+
+        return mapper.toDto(response);
     }
 
-    public void handleNotification() {
+    public void handleNotification(List<ContactNotificationRequestDto> request) {
         // Lógica para salvar em um banco a informação ou entregar a outra aplicação
+        LOGGER.info(request.toString());
     }
 }
