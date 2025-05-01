5e6d8bdec55227c17ba0575ffb58a4db292fff06
romulomotta <romulomotta@outlook.com>
2025-05-01 01:43:44 -0300

    Preparei chamada com retry

--- src/main/java/com/lobato/desafiomeetime/repository/Integration/ContactIntegration.java
+++ src/main/java/com/lobato/desafiomeetime/repository/Integration/ContactIntegration.java
@@ -1,35 +1,49 @@
 package com.lobato.desafiomeetime.repository.Integration;
 
 import com.lobato.desafiomeetime.application.ContactMapper;
+import com.lobato.desafiomeetime.application.TokenMapper;
 import com.lobato.desafiomeetime.application.domain.ContactRequestDomain;
+import com.lobato.desafiomeetime.application.domain.TokenResponseDomain;
+import com.lobato.desafiomeetime.config.token.TokenIntegration;
 import com.lobato.desafiomeetime.repository.client.ContactClient;
 import com.lobato.desafiomeetime.repository.entity.ContactResponseEntity;
 import feign.FeignException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.HttpServerErrorException;
 import org.springframework.web.client.RestClientException;
+
+import static com.lobato.desafiomeetime.config.utils.HeaderUtils.createHeaderWithBearer;
 
 @Service
 public class ContactIntegration {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(ContactIntegration.class);
     private final ContactMapper mapper;
+    private final TokenMapper tokenMapper;
     private final ContactClient client;
+    private final TokenIntegration tokenIntegration;
 
-    public ContactIntegration(ContactMapper mapper, ContactClient client) {
+    public ContactIntegration(ContactMapper mapper, TokenMapper tokenMapper, ContactClient client, TokenIntegration tokenIntegration) {
         this.mapper = mapper;
+        this.tokenMapper = tokenMapper;
         this.client = client;
+        this.tokenIntegration = tokenIntegration;
     }
 
+    @Retryable(include = {FeignException.TooManyRequests.class},
+            maxAttempts = 2, backoff = @Backoff(delay = 11000))
     public ContactResponseEntity createContact(ContactRequestDomain domain) {
         try {
-            return client.createContact(createHeaders(domain.token()), mapper.toEntity(domain));
+            return client.createContact(createHeaderWithBearer(domain.token()), mapper.toEntity(domain));
         } catch (FeignException.FeignClientException ex) {
+            if (ex.status() == HttpStatus.UNAUTHORIZED.value()) {
+                retryWithUpdatedToken(domain, updateToken(domain));
+            }
             LOGGER.error("Erro 4xx, ao buscar Token Access. %s".formatted(ex.contentUTF8()));
             throw new RestClientException("Erro de cliente na API de Token", ex);
         } catch (FeignException.FeignServerException ex) {
@@ -38,11 +52,14 @@ public class ContactIntegration {
         }
     }
 
-    private HttpHeaders createHeaders(String token) {
-        HttpHeaders headers = new HttpHeaders();
-        headers.add(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE);
-        headers.add(HttpHeaders.AUTHORIZATION, "Bearer %s".formatted(token));
+    private void retryWithUpdatedToken(ContactRequestDomain requestDomain, TokenResponseDomain responseDomain) {
+        ContactRequestDomain updatedRequest = new ContactRequestDomain(
+                responseDomain.accessToken(), responseDomain.refreshToken(),
+                requestDomain.email(), requestDomain.lastname(), requestDomain.firstname());
+        createContact(updatedRequest);
+    }
 
-        return headers;
+    private TokenResponseDomain updateToken(ContactRequestDomain domain) {
+        return tokenIntegration.getToken(tokenMapper.toRequestToken(domain, true));
     }
 }
