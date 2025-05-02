d75bb6304511a2d86ceddbdcfef4ca5fe8cce93c
romulomotta <romulomotta@outlook.com>
2025-05-02 13:32:20 -0300

    add test accesstoken

--- src/test/java/com/lobato/desafiomeetime/integration/TokenIntegrationTest.java
+++ src/test/java/com/lobato/desafiomeetime/integration/TokenIntegrationTest.java
@@ -1,15 +1,17 @@
 package com.lobato.desafiomeetime.integration;
 
+import com.github.tomakehurst.wiremock.client.WireMock;
 import com.lobato.desafiomeetime.ApplicationTests;
 import com.lobato.desafiomeetime.config.properties.HubSpotProperties;
+import com.lobato.desafiomeetime.config.utils.JsonUtils;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 public class TokenIntegrationTest extends ApplicationTests {
 
@@ -29,5 +31,24 @@ public class TokenIntegrationTest extends ApplicationTests {
                 .andExpect(status().isFound())
                 .andExpect(header().string("Location", URL_REDIRECT
                         .formatted(properties.getClientId(), properties.getRedirectUri(), properties.getScope())));
+    }
+
+    @Test
+    @DisplayName("Teste de Obter Access Token")
+    void testGetAccessToken() throws Exception {
+
+        WireMock.stubFor(WireMock.post(WireMock.urlPathMatching("/oauth/v1/token"))
+                .willReturn(WireMock.aResponse()
+                        .withStatus(200)
+                        .withHeader("Content-Type", MediaType.APPLICATION_JSON_VALUE)
+                        .withBody(JsonUtils
+                                .getJsonContentFromFile("external/responses", "access-token"))));
+
+
+        mockMvc.perform(get("/autenticar/auth-callback")
+                        .param("code", "mockCode"))
+                .andExpect(status().isCreated())
+                .andExpect(content().json(JsonUtils
+                        .getJsonContentFromFile("internal/responses", "access-token")));
     }
 }
