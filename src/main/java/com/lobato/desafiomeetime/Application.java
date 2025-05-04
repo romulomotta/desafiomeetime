7c910a3449e8d8a6221175f1995f49e506933083
romulomotta <romulomotta@outlook.com>
2025-05-03 21:59:28 -0300

    enable retry

--- src/main/java/com/lobato/desafiomeetime/Application.java
+++ src/main/java/com/lobato/desafiomeetime/Application.java
@@ -3,9 +3,11 @@ package com.lobato.desafiomeetime;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.retry.annotation.EnableRetry;
 
 @SpringBootApplication(scanBasePackages = {"com.lobato.desafiomeetime.*"})
 @EnableFeignClients
+@EnableRetry
 public class Application {
 
 	public static void main(String[] args) {
